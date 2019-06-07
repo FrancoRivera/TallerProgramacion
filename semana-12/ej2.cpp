33f6eca10963245f2a46cab729ced6ec467b8d9c
Franco Rivera <franco@riverarivas.com>
2019-06-07 16:01:22 -0500

    starting ej2 semana 12

--- semana-12/ej2.cpp
+++ semana-12/ej2.cpp
@@ -1,3 +1,27 @@
 
 // ejercicio 2.cpp
 //
+//
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct Carro{
+  int serie; //serie de 6 digitos
+  char modelo;
+  char color;
+};
+
+int main(){
+
+  vector<Carro> carros;
+
+  Carro carro = {123456, 'F', 'A'};
+  carros.push_back(carro);
+
+  return 0;
+}
