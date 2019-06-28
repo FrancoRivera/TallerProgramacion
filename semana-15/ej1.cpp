1bbdc6099eb4f59b215f33acef525e262f7c12b3
Franco Rivera <franco@riverarivas.com>
2019-06-28 13:34:47 -0500

    new matrix

--- semana-15/ej1.cpp
+++ semana-15/ej1.cpp
@@ -20,22 +20,50 @@ using namespace std;
 // matriz 20x20
 void genera_muestra_matriz(int matriz[20][20]){
 
-  // generar matriz
+  int mapa [20][20] = {
+  // 0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5  6  7  8  9
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, // 0
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 1
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 2
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 3
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1}, // 4
+    {1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 5
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 6
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 7
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 8
+    {1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 9
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 0
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 1
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 2
+    {1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 3
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}, // 4
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}, // 5
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 6
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 7
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, // 8
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}  // 9
+  };
+
   for (int i = 0; i < 20; i++) {
     for (int j = 0; j < 20; j++) {
-      matriz[i][j] = 0;
+      matriz[i][j] = mapa[i][j];
     }
   }
 
 }
 
 void imprimir_matriz(int matriz[20][20]){
-
   for (int i = 0; i < 20; i++) {
     for (int j = 0; j < 20; j++) {
-      cout << matriz[i][j];
+      if (matriz[i][j] == 0) cout << " ";// imprime un espacio
+      else{
+	if (matriz[i][j] == 1) cout << "-";// imprime un bloque
+	else{
+	  cout << matriz[i][j];
+	}
+      }
     }
-      cout << endl;
+    cout << endl;
   }
 }
 
@@ -53,19 +81,19 @@ void imprimir_matriz(int matriz[20][20]){
 // dir = direccion actual (1 o -1)
 void desplazar_ficha(int matriz[20][20], int mov, int &pos_x, int &pos_y, int &dir){
 
-   // reseteamos el valor de la matriz
-   matriz[pos_x][pos_y] = 0;
+  // reseteamos el valor de la matriz
+  matriz[pos_x][pos_y] = 0;
 
-   if (mov == 0){
-     pos_x+=dir;
-     if (pos_x+dir > 20 || pos_x+dir < 0 || pos_x+dir == 1) dir *= -1;
-   }
-   if (mov == 1){
-     pos_y+=dir;
-     if (pos_y+dir > 20 || pos_y+dir < 0 || pos_y+dir == 1) dir *= -1;
-   }
+  if (mov == 0){
+    pos_x += dir;
+    if (pos_x+dir > 20 || pos_x + dir < 0 || (pos_x + dir) == 1) dir *= -1;
+  }
+  if (mov == 1){
+    pos_y += dir;
+    if (pos_y+dir > 20 || pos_y + dir < 0 || (pos_y + dir) == 1) dir *= -1;
+  }
 
-   matriz[pos_x][pos_y] = 2;
+  matriz[pos_x][pos_y] = 2;
 }
 
 int main(){
@@ -78,13 +106,11 @@ int main(){
   //
   genera_muestra_matriz(matriz);
 
-
-
   //solicitar posicion inicial (x, y)
   cout << "Posicion inicial de la ficha" << endl;
-  cout << "X:" << endl;
+  cout << "X:";
   cin >> pos_x;
-  cout << "Y:" << endl;
+  cout << "Y:";
   cin >> pos_y;
 
 
@@ -115,5 +141,5 @@ int main(){
   }
 
 
-return 0;
+  return 0;
 }
